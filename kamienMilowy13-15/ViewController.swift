46dff53f723c83ecbc1fcda23aa6d3a5fc02c670
MarcinMatuk <marcin.mates@gmail.com>
2021-10-02 13:26:20 +0200

    started

---
+++ kamienMilowy13-15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kamienMilowy13-15
+//
+//  Created by Marcin Matuk on 02/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
